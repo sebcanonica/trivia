5ee49a267b218b8c929741767156ea02ba4f1348
Sébastien Canonica <sebcano.biz@laposte.net>
2020-09-29 21:15:09 +0200

    Fix garbage commit

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/PlayerMoved.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/PlayerMoved.java
@@ -1,2 +1,9 @@
 package com.adaptionsoft.games.uglytrivia;public class PlayerMoved {
+    public final int newLocation;
+    public final String name;
+
+    public PlayerMoved(int newLocation, String name) {
+        this.newLocation = newLocation;
+        this.name = name;
+    }
 }
